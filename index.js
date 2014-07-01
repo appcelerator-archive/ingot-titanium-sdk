c071c5902cf0811a1cddcd3ad473f9b678952080
Chris Barber <chris@cb1inc.com>
2014-07-01 15:25:25 -0700

    Broke out the handlers into separate files.

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 exports.init = function init(spoke, next) {
-	// titanium.repo.update
-	// titanium.package.sdk
-	// titanium.build.platform
-	// titanium.test.branch
-	// titanium.test.release
-
-	next(null, {});
-};
-
-exports.run = function run(next) {
-	next();
+	spoke.registerJobHandler('titanium.repo.update', require('./lib/repo-update'));
+	spoke.registerJobHandler('titanium.package.sdk', require('./lib/package-sdk'));
+	spoke.registerJobHandler('titanium.build.platform', require('./lib/build-platform'));
+	spoke.registerJobHandler('titanium.test.branch', require('./lib/test-branch'));
+	spoke.registerJobHandler('titanium.test.release', require('./lib/test-release'));
+	next(null, {
+		/* titanium sdk specific capabilities */
+	});
 };
